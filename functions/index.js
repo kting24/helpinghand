5f38b7b1947788f6748f1347c4c0a7da54688612
Moises David Lopez Mendoza <mdlopezme@gmail.com>
2021-02-19 23:31:52 -0800

    Initial Commit

---
+++ functions/index.js
@@ -0,0 +1,20 @@
+// const functions = require("firebase-functions");
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", { structuredData: true });
+//   response.send("Hello from Firebase!");
+// });
+
+// index.js
+
+const functions = require("firebase-functions");
+const app = require("express")();
+
+const { getAllTodos } = require("./APIs/todos");
+
+app.get("/todos", getAllTodos);
+
+exports.api = functions.https.onRequest(app);
