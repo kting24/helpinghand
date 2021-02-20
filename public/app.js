e9f4b508f83e0d3c803093b869369468bd9ec05b
Moises David Lopez Mendoza <mdlopezme@gmail.com>
2021-02-20 15:44:50 -0800

    sure

--- public/app.js
+++ public/app.js
@@ -14,23 +14,23 @@ auth.onAuthStateChanged((firebaseUser) => {});
 
 // /// Sign in event handlers
 
-// signInBtn.onclick = () => auth.signInWithPopup(provider);
+signInBtn.onclick = () => auth.signInWithPopup(provider);
 
-// signOutBtn.onclick = () => auth.signOut();
+signOutBtn.onclick = () => auth.signOut();
 
-// auth.onAuthStateChanged((user) => {
-//   if (user) {
-//     // signed in
-//     whenSignedIn.hidden = false;
-//     whenSignedOut.hidden = true;
-//     userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
-//   } else {
-//     // not signed in
-//     whenSignedIn.hidden = true;
-//     whenSignedOut.hidden = false;
-//     userDetails.innerHTML = "";
-//   }
-// });
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    // signed in
+    whenSignedIn.hidden = false;
+    whenSignedOut.hidden = true;
+    userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
+  } else {
+    // not signed in
+    whenSignedIn.hidden = true;
+    whenSignedOut.hidden = false;
+    userDetails.innerHTML = "";
+  }
+});
 
 // ///// Firestore /////
 
