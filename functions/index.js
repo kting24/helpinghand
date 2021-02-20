b1bb8d08db7a4083cf311c9e379da78c8bd820b2
Moises David Lopez Mendoza <mdlopezme@gmail.com>
2021-02-20 00:30:38 -0800

    Dinamic webstie

--- functions/index.js
+++ functions/index.js
@@ -11,10 +11,69 @@
 // index.js
 
 const functions = require("firebase-functions");
-const app = require("express")();
+const express = require("express");
+const engines = require("consolidate");
+var hbs = require("handlebars");
 
-const { getAllTodos } = require("./APIs/todos");
+const app = express();
+app.engine("hbs", engines.handlebars);
+app.set("views", "./views");
+app.set("view engine", "hbs");
 
-app.get("/todos", getAllTodos);
+var admin = require("firebase-admin");
 
-exports.api = functions.https.onRequest(app);
+// var serviceAccount = require("/home/moises/sdhacks/key.json");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://sd-hacks-2021-default-rtdb.firebaseio.com",
+// });
+
+admin.initializeApp(functions.config().firebase);
+
+async function getFirestore() {
+  const firestore_con = await admin.firestore();
+  const writeResult = firestore_con
+    .collection("sample")
+    .doc("sample_id")
+    .get()
+    .then((doc) => {
+      if (!doc.exists) {
+        console.log("No such document!");
+      } else {
+        return doc.data();
+      }
+    })
+    .catch((err) => {
+      console.log("Error getting document", err);
+    });
+  return writeResult;
+}
+
+// Create route and send results to front end
+app.get("/", async (request, response) => {
+  var db_result = await getFirestore();
+  response.render("index", { db_result });
+});
+exports.app = functions.https.onRequest(app);
+
+async function insertFormData(request) {
+  const writeResult = await admin
+    .firestore()
+    .collection("form_data")
+    .add({
+      firstname: request.body.firstname,
+      lastname: request.body.lastname,
+    })
+    .then(function () {
+      console.log("Document successfully written!");
+    })
+    .catch(function (error) {
+      console.error("Error writing document: ", error);
+    });
+}
+
+app.post("/insert_data", async (request, response) => {
+  var insert = await insertFormData(request);
+  response.sendStatus(200);
+});
